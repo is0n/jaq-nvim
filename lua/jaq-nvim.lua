1108d615652321492a44011b38179b9ed202d60b
Luis Palacios <paluisito2007@gmail.com>
2022-04-18 18:05:44 -0700

    Improve mapping and window management

--- lua/jaq-nvim.lua
+++ lua/jaq-nvim.lua
@@ -36,20 +36,20 @@ local config = {
 }
 
 local function floatingWin(cmd)
-	local buf = vim.api.nvim_create_buf(false, true)
-	vim.api.nvim_buf_set_keymap(buf, 'n', '<ESC>', '<C-\\><C-n>:lua vim.api.nvim_win_close(win, true)<CR>', { silent = true })
-	vim.api.nvim_buf_set_option(buf, 'filetype', 'Jaq')
+	M.buf = vim.api.nvim_create_buf(false, true)
 	local win_height = math.ceil(vim.api.nvim_get_option("lines") * config.ui.float.height - 4)
 	local win_width = math.ceil(vim.api.nvim_get_option("columns") * config.ui.float.width)
 	local row = math.ceil((vim.api.nvim_get_option("lines") - win_height) * config.ui.float.y - 1)
 	local col = math.ceil((vim.api.nvim_get_option("columns") - win_width) * config.ui.float.x)
 	local opts = { style = "minimal", relative = "editor", border = config.ui.float.border, width = win_width, height = win_height, row = row, col = col }
-	local win = vim.api.nvim_open_win(buf, true, opts)
+	M.win = vim.api.nvim_open_win(M.buf, true, opts)
+	vim.api.nvim_buf_set_option(M.buf, 'filetype', 'Jaq')
+	vim.api.nvim_buf_set_keymap(M.buf, 'n', '<ESC>', '<C-\\><C-n>:lua vim.api.nvim_win_close(' .. M.win .. ', true)<CR>', { silent = true })
 	vim.fn.termopen(cmd)
 	if config.ui.startinsert then vim.cmd("startinsert") end
 	if config.ui.wincmd then vim.cmd("wincmd p") end
-	vim.api.nvim_win_set_option(win, 'winhl', 'Normal:' .. config.ui.float.float_hl .. ',FloatBorder:' .. config.ui.float.border_hl)
-	vim.api.nvim_win_set_option(win, 'winblend', config.ui.float.blend)
+	vim.api.nvim_win_set_option(M.win, 'winhl', 'Normal:' .. config.ui.float.float_hl .. ',FloatBorder:' .. config.ui.float.border_hl)
+	vim.api.nvim_win_set_option(M.win, 'winblend', config.ui.float.blend)
 end
 
 function M.setup(user_options) config = vim.tbl_deep_extend('force', config, user_options) end
@@ -77,9 +77,9 @@ local function run(type)
 			if config.ui.wincmd then vim.cmd("wincmd p") end
 		elseif type == "term" or type == "terminal" then
 			vim.cmd(config.ui.terminal.position .. " " .. config.ui.terminal.size .. "new | term " .. cmd)
-			local buf = vim.api.nvim_get_current_buf()
-			vim.api.nvim_buf_set_keymap(buf, 'n', '<ESC>', '<C-\\><C-n>:bdelete!<CR>', { silent = true })
-			vim.api.nvim_buf_set_option(buf, 'filetype', 'Jaq')
+			M.buf = vim.api.nvim_get_current_buf()
+			vim.api.nvim_buf_set_keymap(M.buf, 'n', '<ESC>', '<C-\\><C-n>:bdelete!<CR>', { silent = true })
+			vim.api.nvim_buf_set_option(M.buf, 'filetype', 'Jaq')
 			if config.ui.startinsert then vim.cmd("startinsert") end
 			if config.ui.wincmd then vim.cmd("wincmd p") end
 			if not config.ui.terminal.line_no then vim.cmd("setlocal nonumber") vim.cmd("setlocal norelativenumber") end
