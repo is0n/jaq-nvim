bbca7b6b4e2839174d54d31cc3a344739e7a526a
TomZz <pythongolangkotlinrust@gmail.com>
2022-11-18 13:31:52 +0800

    Update jaq-nvim.lua
    Add a new environment varialble to solve #22

--- lua/jaq-nvim.lua
+++ lua/jaq-nvim.lua
@@ -150,6 +150,7 @@ local function substitute(cmd)
   cmd = cmd:gsub("%%", vim.fn.expand('%'));
   cmd = cmd:gsub("$fileBase", vim.fn.expand('%:r'));
   cmd = cmd:gsub("$filePath", vim.fn.expand('%:p'));
+  cmd = cmd:gsub("$baseName", vim.fn.expand("%:t:r"))
   cmd = cmd:gsub("$file", vim.fn.expand('%'));
   cmd = cmd:gsub("$dir", vim.fn.expand('%:p:h'));
   cmd = cmd:gsub("$moduleName",
