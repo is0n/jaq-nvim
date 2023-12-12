0c411866c92dc805f5a7eb6963542f43443516ed
akotro <akotronakis@hotmail.com>
2023-12-12 12:48:30 +0200

    chore(formatting): 4 space formatting

--- plugin/jaq-nvim.lua
+++ plugin/jaq-nvim.lua
@@ -1,30 +1,30 @@
 vim.cmd([[
-	function! JaqCompletion(lead, cmd, cursor)
-		let valid_args = [ 'internal', 'float', 'terminal', 'bang', 'quickfix' ]
-		let l = len(a:lead) - 1
-		if l >= 0
-			let filtered_args = copy(valid_args)
-			call filter(filtered_args, {_, v -> v[:l] ==# a:lead})
-			if !empty(filtered_args)
-				return filtered_args
-			endif
-		endif
-		return valid_args
-	endfunction
+    function! JaqCompletion(lead, cmd, cursor)
+        let valid_args = [ 'internal', 'float', 'terminal', 'bang', 'quickfix' ]
+        let l = len(a:lead) - 1
+        if l >= 0
+            let filtered_args = copy(valid_args)
+            call filter(filtered_args, {_, v -> v[:l] ==# a:lead})
+            if !empty(filtered_args)
+                return filtered_args
+            endif
+        endif
+        return valid_args
+    endfunction
 
-	function! JaqRunCompletion(lead, cmd, cursor)
-		let valid_args = [ 'float', 'terminal', 'bang', 'quickfix' ]
-		let l = len(a:lead) - 1
-		if l >= 0
-			let filtered_args = copy(valid_args)
-			call filter(filtered_args, {_, v -> v[:l] ==# a:lead})
-			if !empty(filtered_args)
-				return filtered_args
-			endif
-		endif
-		return valid_args
-	endfunction
+    function! JaqRunCompletion(lead, cmd, cursor)
+        let valid_args = [ 'float', 'terminal', 'bang', 'quickfix' ]
+        let l = len(a:lead) - 1
+        if l >= 0
+            let filtered_args = copy(valid_args)
+            call filter(filtered_args, {_, v -> v[:l] ==# a:lead})
+            if !empty(filtered_args)
+                return filtered_args
+            endif
+        endif
+        return valid_args
+    endfunction
 
-	command! -nargs=? -complete=customlist,JaqCompletion Jaq :lua require('jaq-nvim').Jaq(<f-args>)
-	command! -nargs=? -complete=customlist,JaqRunCompletion JaqRun :lua require('jaq-nvim').JaqRun(<f-args>)
+    command! -nargs=? -complete=customlist,JaqCompletion Jaq :lua require('jaq-nvim').Jaq(<f-args>)
+    command! -nargs=? -complete=customlist,JaqRunCompletion JaqRun :lua require('jaq-nvim').JaqRun(<f-args>)
 ]])
